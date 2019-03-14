d235a0e066f92255aac841afc1c5e0238e72aa27
Shamit <s.rajbansi@gmail.com>
2019-03-14 23:58:55 +0200

    Added drawer: + username (Text only) + email address (Text only)
    Added comments to appbar and drawer

--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,38 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+//==========================================================================================================
+// HEADER - APPBAR
+//==========================================================================================================
+
       appBar: new AppBar(
         elevation: 0.1,
         backgroundColor: Colors.transparent,
-        title: Text("Instant Catalogue",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
+        title: Text("Instant Catalogue",
+          style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
         actions: <Widget>[
           new IconButton(icon: Icon(Icons.assignment),color: Colors.black, onPressed: (){}),
         ],
       ),
+
+//==========================================================================================================
+// Slide-in Drawer: Containing options and feature of app, account, notifications, settings etc.
+//==========================================================================================================
+
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+
+// Header of app drawer
+            new UserAccountsDrawerHeader(
+                accountName: Text("Username"),
+                accountEmail: Text("User@somemail.com")
+            ),
+          ],
+        ),
+      ),
+
     );
-
-
   }
 }
