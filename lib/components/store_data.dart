ba8abfa096675d695281bec736cdd99f4c50c93a
Shamit <s.rajbansi@gmail.com>
2019-11-04 13:01:18 +0200

    - Added store images to cards.

--- lib/components/store_data.dart
+++ lib/components/store_data.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
 class stores {
+  final String storeImage;
   final String title;
   final DateTime startDate;
   final DateTime endDate;
   final double budget;
 
-  stores(this.title, this.startDate, this.endDate, this.budget);
+
+  stores(this.storeImage, this.title, this.startDate, this.endDate, this.budget,);
 }
--- lib/components/store_listing.dart
+++ lib/components/store_listing.dart
@@ -4,13 +4,13 @@ import 'store_data.dart';
 
 class StoreListing extends StatelessWidget {
   final List<stores> storesList = [
-    stores("Pick n Pay", DateTime.now(), DateTime.now(), 200.00),
-    stores("Checkers", DateTime.now(), DateTime.now(), 200.00),
-    stores("Game", DateTime.now(), DateTime.now(), 200.00),
-    stores("Spar", DateTime.now(), DateTime.now(), 200.00),
-    stores("Woolworths", DateTime.now(), DateTime.now(), 200.00),
-    stores("Dion Wired", DateTime.now(), DateTime.now(), 200.00),
-    stores("Incredible Connection", DateTime.now(), DateTime.now(), 200.00),
+    stores("images/pnplogo.png", "Pick n Pay", DateTime.now(), DateTime.now(), 200.00),
+    stores("images/checkerslogo.png", "Checkers", DateTime.now(), DateTime.now(), 200.00),
+    stores("images/sparlogo.png", "Game", DateTime.now(), DateTime.now(), 200.00),
+    stores("images/gamelogo.png", "Spar", DateTime.now(), DateTime.now(), 200.00),
+//    stores("images/pnplogo.png", "Woolworths", DateTime.now(), DateTime.now(), 200.00),
+//    stores("images/pnplogo.png", "Dion Wired", DateTime.now(), DateTime.now(), 200.00),
+//    stores("images/pnplogo.png", "Incredible Connection", DateTime.now(), DateTime.now(), 200.00),
 
   ];
 
@@ -23,47 +23,53 @@ class StoreListing extends StatelessWidget {
       ),
     );
   }
-  
+
   Widget buildStoreListCard(BuildContext context, int index){
     final stores = storesList[index];
     return new Container(
-        child: Card(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: <Widget>[
 
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-                    child: Row(
-                      children: <Widget>[
-                        Text(stores.title, style: new TextStyle(fontSize: 17.0),),
-                        Spacer(),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-                    child: Row(
-                      children: <Widget>[
-                        Text("${DateFormat('dd/MM/yyyy').format(stores.startDate).toString()} - ${DateFormat('dd/MM/yyyy').format(stores.endDate).toString()}"),
-                        Spacer(),
-                      ],
-                    ),
-                  ),
+          child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(11.0),
+                  child: Column(
+                    children: <Widget>[
 
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-                    child: Row(
-                      children: <Widget>[
-                        Text(stores.budget.toString(), textAlign: TextAlign.right,),
-                      ],
-                    ),
-                  )
-                ],
+                      InkWell(
+                        onTap: (){},
+                        child: Image.asset(stores.storeImage),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                          child: Row(
+                            children: <Widget>[
+                              Text(stores.title, style: new TextStyle(fontSize: 17.0),),
+                              Spacer(),
+                            ],
+                          ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text("${DateFormat('dd/MM/yyyy').format(stores.startDate).toString()} - ${DateFormat('dd/MM/yyyy').format(stores.endDate).toString()}"),
+                            Spacer(),
+                          ],
+                        ),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text(stores.budget.toString(), textAlign: TextAlign.right,),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
               ),
-            )
-        )
+          ),
     );
   }
 }
